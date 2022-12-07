953c70603e0b395f276fd1a0e7e33d87adf92484
FranyxD <franyx2001@gmail.com>
2022-12-07 18:40:51 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      
+      extend: {
+        backgroundImage: {
+          //'light-pattern': "url('./images/pattern-light.svg')",
+          //'dark-pattern': "url('./images/pattern-dark.svg')",
+        },
+        screens: {
+          '3xl': '1850px',
+        },
+        colors: {
+          //'black': '#1D1D1D',
+          //'white': '#f0f0f0',
+        },
+        fontFamily: {
+          //'italiana': ['Italiana']
+        },
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
